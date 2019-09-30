daf7d57c60e78b55dc8e004b2bac5195e1711aee
simaung <ddhr00@gmail.com>
2019-09-30 13:35:18 +0700

    request data pada laravel

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@ Route::get('/', function () {
 });
 
 Route::get('dosen', 'DosenController@index');
+
+Route::get('/pegawai/{nama}', 'PegawaiController@index');
+
+Route::get('/formulir', 'PegawaiController@formulir');
+
+Route::post('/formulir/proses', 'PegawaiController@proses');
